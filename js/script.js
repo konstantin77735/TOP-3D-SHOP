681245b2368cc15547f6b3a21eb991e45663e8a4
Konstantin <konstantinstepanyan99@gmail.com>
2021-02-11 06:10:45 +0300

    add hover effect for svgs in layout

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const linksToHover = document.querySelectorAll('.better-to-see__link');
+
+
+    for (let i of linksToHover) {
+
+
+        for (let child of i.children) {
+            
+            console.log(child);
+            if (child.classList.contains("better-to-see__arrow")) {
+                i.addEventListener('mouseover', () => {
+                    child.style.fill = "#D2001C"
+                });
+                i.addEventListener('mouseout', () => {
+                    child.style.fill = "#4D4D4D"
+                });
+            }
+        }
+    }
+
+
     class Burger {
 
         constructor(settings) {
@@ -9,43 +30,43 @@ document.addEventListener('DOMContentLoaded', () => {
             this.isChanging = false;
 
             this.headerH;
-            
+
             this.init();
         }
 
 
         init() {
-            
+
             this.headerH = this.header.offsetHeight;
-            
+
             const checkClientWidth = () => {
-                
+
                 if (window.innerWidth <= 900) {
                     this.nav.style.height = 0;
                     this.nav.style.margin = 0;
 
                     this.ham.style.display = 'block';
-                    
+
                     this.nav.classList.remove("menu_show");
                     this.ham.classList.remove("burger_close");
-                    
+
                     document.querySelector('.header').style.height = '200px'; //200 - default value from css
-                    
-                    
+
+
                 } else {
-                    
+
                     this.nav.style.margin = '0 auto';
                     this.ham.style.display = 'none';
-                    
+
                     this.nav.classList.add("menu_show");
                     this.ham.classList.add("burger_close");
-                    
+
                     document.querySelector('.header').style.height = '239px'; //239 - default value from css
-                    
-                    
+
+
                 }
             };
-            
+
             checkClientWidth();
 
             window.addEventListener('resize', () => {
@@ -66,9 +87,9 @@ document.addEventListener('DOMContentLoaded', () => {
         toggleHamburger() {
             console.log(this.headerH);
             if (!this.isChanging) {
-                
+
                 if (!this.ham.classList.contains("burger_close")) {
-                    this.nav.height=0;
+                    this.nav.height = 0;
                     this.header.style.height = '400px';
                 } else {
                     console.log(this.headerH);
@@ -77,7 +98,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 this.nav.classList.toggle("menu_show");
                 this.ham.classList.toggle("burger_close");
-                
+
                 this.isChanging = true;
             }
 
